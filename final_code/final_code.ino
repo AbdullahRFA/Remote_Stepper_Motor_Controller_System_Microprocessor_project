4aae0783243e60b68510bb6a1ab82c05589d1f77
AbdullahRFA <shakibrybmn@gmail.com>
2025-06-24 00:01:21 +0600

    added final code

---
+++ final_code/final_code.ino
@@ -0,0 +1,223 @@
+// Project: Remote Stepper Motor Controller System
+// Author: AbdullahRFA
+
+#include <IRremote.hpp>
+#include <AccelStepper.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+#include <EEPROM.h>
+
+// === OLED DISPLAY SETUP ===
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 64
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
+
+// === IR Remote ===
+#define IR_RECEIVER_PIN 4
+#define LED_FEEDBACK_PIN 2
+int state = 1;
+
+// === IR Button Command Values (Decimal) ===
+int forward = 17;
+int backword = 16;
+int speed_increase = 19;
+int speed_decrease = 18;
+int stopMotor = 11;
+int idlePosition = 28;
+
+// === Stepper Motor Pins ===
+#define MOTOR_PIN1 14
+#define MOTOR_PIN2 12
+#define MOTOR_PIN3 13
+#define MOTOR_PIN4 15
+AccelStepper stepper(AccelStepper::HALF4WIRE, MOTOR_PIN1, MOTOR_PIN3, MOTOR_PIN2, MOTOR_PIN4);
+
+// === Variables ===
+int speed = 300;
+bool motorActive = false;
+bool moveForward = false;
+bool moveBackward = false;
+bool previousDirectionForward = true;
+bool idleToggleState = false;
+
+int stepRequest = 0; // New: Store number key pressed for cycle request
+bool cycleAwaitDirection = false;
+
+// EEPROM Addresses
+#define EEPROM_SPEED_ADDR 0
+#define EEPROM_DIR_ADDR 4
+
+void saveToEEPROM() {
+  EEPROM.writeInt(EEPROM_SPEED_ADDR, speed);
+  EEPROM.writeBool(EEPROM_DIR_ADDR, previousDirectionForward);
+  EEPROM.commit();
+}
+
+void loadFromEEPROM() {
+  speed = EEPROM.readInt(EEPROM_SPEED_ADDR);
+  previousDirectionForward = EEPROM.readBool(EEPROM_DIR_ADDR);
+  if (speed < 100 || speed > 1000) speed = 300;
+}
+
+void displayStatus(const char* action) {
+  display.clearDisplay();
+  display.setCursor(0, 0);
+  display.setTextSize(1);
+  display.println(action);
+
+  display.setCursor(0, 20);
+  display.print("Speed: "); display.println(speed);
+
+  display.setCursor(0, 30);
+  display.print("Direction: ");
+  display.println(previousDirectionForward ? "Forward" : "Backward");
+
+  display.setCursor(0, 50);
+  display.print("State: ");
+  display.println(motorActive ? "Running" : "Stopped");
+  display.display();
+}
+
+void showCommandFeedback(int command) {
+  display.clearDisplay();
+  display.setCursor(0, 0);
+  display.setTextSize(2);
+  display.print("Cmd: ");
+  display.setCursor(0, 20);
+  display.println(command);
+  display.display();
+  delay(250);
+}
+
+void moveOneCycle(bool dir) {
+  stepper.setSpeed(dir ? abs(speed) : -abs(speed));
+  int steps = 2048; // one cycle for 28BYJ-48
+  stepper.move(steps * (dir ? 1 : -1));
+  while (stepper.distanceToGo() != 0) {
+    stepper.run();
+  }
+  displayStatus(dir ? "One Cycle Forward" : "One Cycle Backward");
+}
+
+void setup() {
+  Serial.begin(115200);
+
+  if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
+    Serial.println(F("SSD1306 allocation failed"));
+    while (true);
+  }
+  display.clearDisplay();
+  display.setTextSize(1);
+  display.setTextColor(SSD1306_WHITE);
+  display.setCursor(0, 0);
+  display.println("Initializing...");
+  display.display();
+
+  EEPROM.begin(64);
+  loadFromEEPROM();
+  displayStatus("Stepper Ready");
+
+  IrReceiver.begin(IR_RECEIVER_PIN, ENABLE_LED_FEEDBACK);
+  pinMode(LED_FEEDBACK_PIN, OUTPUT);
+
+  stepper.setMaxSpeed(1000);
+  stepper.setSpeed(speed);
+  stepper.setAcceleration(500);
+}
+
+void loop() {
+  if (IrReceiver.decode()) {
+    int command = IrReceiver.decodedIRData.command;
+    Serial.print("IR Command: "); Serial.println(command);
+
+    showCommandFeedback(command);
+
+    if (command >= 1 && command <= 9) {
+      stepRequest = command;
+      cycleAwaitDirection = true;
+      displayStatus("Press F/B for Cycle");
+    } else if (cycleAwaitDirection && (command == forward || command == backword)) {
+      bool dir = (command == forward);
+      for (int i = 0; i < stepRequest; i++) {
+        moveOneCycle(dir);
+      }
+      cycleAwaitDirection = false;
+      stepRequest = 0;
+    } else {
+      switch (command) {
+        case 17:
+          moveForward = true;
+          moveBackward = false;
+          motorActive = true;
+          previousDirectionForward = true;
+          idleToggleState = false;
+          displayStatus("Moving Forward");
+          break;
+        case 16:
+          moveBackward = true;
+          moveForward = false;
+          motorActive = true;
+          previousDirectionForward = false;
+          idleToggleState = false;
+          displayStatus("Moving Backward");
+          break;
+        case 19:
+          speed += 100;
+          if (speed > 1000) speed = 1000;
+          stepper.setSpeed(speed);
+          displayStatus("Speed Increased");
+          break;
+        case 18:
+          speed -= 100;
+          if (speed < 100) speed = 100;
+          stepper.setSpeed(speed);
+          displayStatus("Speed Decreased");
+          break;
+        case 11:
+          moveForward = false;
+          moveBackward = false;
+          motorActive = false;
+          idleToggleState = false;
+          stepper.stop();
+          displayStatus("Motor Stopped");
+          break;
+        case 28:
+          if (!idleToggleState) {
+            motorActive = false;
+            idleToggleState = true;
+            displayStatus("Paused (Idle)");
+          } else {
+            motorActive = true;
+            if (previousDirectionForward) {
+              moveForward = true;
+              moveBackward = false;
+            } else {
+              moveBackward = true;
+              moveForward = false;
+            }
+            idleToggleState = false;
+            displayStatus("Resumed from Idle");
+          }
+          break;
+        default:
+          displayStatus("Unknown Cmd");
+          break;
+      }
+    }
+
+    saveToEEPROM();
+    IrReceiver.resume();
+    delay(300);
+  }
+
+  if (motorActive) {
+    if (moveForward) {
+      stepper.setSpeed(abs(speed));
+      stepper.runSpeed();
+    } else if (moveBackward) {
+      stepper.setSpeed(-abs(speed));
+      stepper.runSpeed();
+    }
+  }
+}
--- remote_test_code/remote_test_code.ino
+++ remote_test_code/remote_test_code.ino
@@ -1,18 +1,65 @@
-#include <IRremote.h>
+#include <IRremote.hpp>
 
-#define IR_RECEIVE_PIN 2  // Connect OUT pin of VS1838 here
+// button value in descimal
+int off = 11;
+int backword = 16;
+int forward = 17;
+int speed_decrease = 18;
+int speed_increase = 19;
+int idlePosition = 28;
+int recall = 23;
+int sys = 12;
+int pp = 14;
+int tv = 15;
+int display = 22;
+int pic = 29;
+int sleep = 21;
+int lock = 24;
+int fav = 26;
+int smode = 30;
+int favscan = 27;
 
-void setup() {
-  Serial.begin(9600);
-  delay(2000);  // Give time to open Serial Monitor
-  IrReceiver.begin(IR_RECEIVE_PIN, ENABLE_LED_FEEDBACK);
-  Serial.println("IR Receiver started on ESP32");
+
+#define IR_RECEIVER_PIN      4                               // IR Receiver pin
+#define LED_PIN              15  
+#define LED_FEEDBACK_PIN 2 
+int state=1;                             // ESP32 inboard pin
+
+void setup()
+{
+    Serial.begin(115200);                                     // Start Serial Monitor
+    IrReceiver.begin(IR_RECEIVER_PIN, ENABLE_LED_FEEDBACK);   // Start IR Receiver, Enable IRreceiver led
+    pinMode(LED_PIN, OUTPUT);
+    digitalWrite(LED_PIN, state);
+    pinMode(LED_FEEDBACK_PIN, OUTPUT);
 }
 
-void loop() {
-  if (IrReceiver.decode()) {
-    Serial.print("IR Code: 0x");
-    Serial.println(IrReceiver.decodedIRData.command, HEX);
-    IrReceiver.resume(); // Ready to receive next code
-  }
+void loop()
+{
+    if (IrReceiver.decode())        // Check if there is a received code
+    {
+       digitalWrite(LED_FEEDBACK_PIN, HIGH);  // Turn LED on for feedback
+        delay(40);                            // Visible duration
+        digitalWrite(LED_FEEDBACK_PIN, LOW);
+
+
+        Serial.print("Received Data: ");
+        Serial.print(IrReceiver.decodedIRData.decodedRawData, DEC);  // Print Received code on Serial Monitor in Hexadecimal
+        Serial.print("  -  Received Command: ");
+        Serial.println(IrReceiver.decodedIRData.command, DEC);  // Print Received command on Serial Monitor in Hexadecimal
+
+        if(IrReceiver.decodedIRData.command == 0x12){   
+          state=!state;     // if you press a button with command number '0'
+          digitalWrite(LED_PIN, state);                       // Turn off the led
+        }
+        if(IrReceiver.decodedIRData.command == 0){
+          Serial.println("Your Press 1");
+        }
+        // if(IrReceiver.decodedIRData.command == idlePosition){
+        //   Serial.println("Your are in idle position");
+        // }
+
+        delay(500);
+        IrReceiver.resume();       // Enable receiving of the next value
+    }
 }
